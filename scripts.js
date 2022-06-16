93384053ff3fad938bd49f9a5a5bdf16f09b5e34
SDMckone <seandmckone@gmail.com>
2022-06-15 22:21:00 -0400

    Add grid functionality and fix errors

--- scripts.js
+++ scripts.js
@@ -1,15 +1,21 @@
 const body = document.querySelector('body');
 const sketchGrid = document.querySelector('#sketchgrid');
+const toolbar = document.querySelector('#toolbar');
 const colorPicker = document.querySelector('.colorpicker');
 const slider = document.querySelector('.slider');
 const output = document.querySelector('.slidervalue');
+const gridButton = document.querySelector('.gridbutton');
 const resetButton = document.querySelector('.resetbutton');
 const eraserButton = document.querySelector('.eraserbutton');
 
-
 let dopaint = false;
 let eraserDown = false;
+let gridOn = false;
 let paintColor = prevColor = colorPicker.value;
+let boxDimension = 800;
+sketchGrid.style.width = `${boxDimension}px`;
+sketchGrid.style.height = `${boxDimension}px`;
+toolbar.style.width = `${boxDimension}px`;
 
 sketchGrid.draggable = false;
 output.textContent = `${slider.value} x ${slider.value}`;
@@ -43,7 +49,6 @@ eraserButton.addEventListener('mousedown', ()=> {
 });
 
 eraserButton.addEventListener('mouseup', ()=> {
-    eraserButton.style.backgroundColor = '#4b4f58';
     eraserButton.style.paddingTop = '1px';
     eraserDown = !eraserDown;
     if(eraserDown) {
@@ -57,8 +62,32 @@ eraserButton.addEventListener('mouseup', ()=> {
     }
 });
 
+gridButton.addEventListener('mousedown', ()=> {
+    if(!gridOn) {
+        gridButton.style.backgroundColor = 'gray';
+    } else {
+        gridButton.style.backgroundColor = '#6890EE';
+    }
+    gridButton.style.paddingTop = '4px';
+});
+
+gridButton.addEventListener('mouseup', ()=> {
+    gridButton.style.backgroundColor = 'lightblue';
+    gridButton.style.paddingTop = '1px';
+    gridOn = !gridOn;
+    if(gridOn) {
+        gridButton.style.backgroundColor = 'blue';
+    } 
+    else {
+        gridButton.style.backgroundColor = '#4b4f58';
+    }
+    toggleGridVisible();
+});
+
 colorPicker.oninput = function() {
     paintColor = colorPicker.value;
+    eraserDown = false;
+    eraserButton.style.backgroundColor = '#4b4f58';
 }
 
 
@@ -74,6 +103,32 @@ slider.onchange = function() {
     newGrid();
 }
 
+function toggleGridVisible() {
+    if(gridOn) {
+        for(let i = 0; i < gridWidth**2; i++){
+            sketchGrid.children[i].style.borderLeft = '1px solid black';
+            sketchGrid.children[i].style.borderTop = '1px solid black';
+            if(i % gridWidth == gridWidth - 1) {
+                sketchGrid.children[i].style.borderRight = '1px solid black';
+            }
+            if(i > gridWidth**2 - gridWidth - 1) {
+                sketchGrid.children[i].style.borderBottom = '1px solid black';
+            }
+        }
+    } else {
+        for(let i = 0; i < gridWidth**2; i++){
+            sketchGrid.children[i].style.borderLeft = '0px';
+            sketchGrid.children[i].style.borderTop = '0px';
+            if(i % gridWidth == gridWidth - 1) {
+                sketchGrid.children[i].style.borderRight = '0px';
+            }
+            if(i > gridWidth**2 - gridWidth - 1) {
+                sketchGrid.children[i].style.borderBottom = '0px';
+            }
+        }
+    }
+}
+
 function newGrid() {
     while (sketchGrid.firstChild) {
         sketchGrid.removeChild(sketchGrid.firstChild);
@@ -81,8 +136,8 @@ function newGrid() {
     for(let i = 0; i < gridWidth**2; i++){
         const griddiv = document.createElement('div');
         griddiv.style.backgroundColor = 'white';
-        griddiv.style.width = `${700/gridWidth}px`
-        griddiv.style.height = `${700/gridWidth}px`
+        griddiv.style.width = `${boxDimension/gridWidth}px`
+        griddiv.style.height = `${boxDimension/gridWidth}px`
         griddiv.classList.add(`griddiv`);
         griddiv.addEventListener("mousedown", function(event){event.preventDefault()});
         griddiv.addEventListener('mouseover', ()=> {
@@ -94,6 +149,9 @@ function newGrid() {
             griddiv.style.backgroundColor = paintColor;
         });
         sketchGrid.append(griddiv);
+    }
+    if(gridOn){
+        toggleGridVisible();
     }
 }
 
